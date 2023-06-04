0565a09f6d9e546a71a762dc8303d88205871276
kamilakirol <kirolkamila@wp.pl>
2023-06-04 19:39:41 +0200

    Initialize

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,12 @@
+import NextAuth from "next-auth";
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+  ],
+});
+
+export { handler as GET, handler as POST };
---
+++ components/LogIn.js
@@ -0,0 +1,13 @@
+const LogIn = () => {
+  return (
+    <div className="bg-blue-900 w-screen h-screen flex items-center">
+      <div className="text-center w-full">
+        <button className="bg-white p-2 px-4 rounded-lg">
+          Login with Google
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default LogIn;
