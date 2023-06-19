dc6d4ab56e203f0b6e3d8df361b9de80f36c3384
kamilakirol <kirolkamila@wp.pl>
2023-06-19 17:45:14 +0200

    Add categories

--- app/api/categories/route.js
+++ app/api/categories/route.js
@@ -1,12 +1,29 @@
+import { mongooseConnect } from "@/lib/mongoose";
 import { Category } from "@/models/Category";
 import { NextResponse } from "next/server";
 
 export async function POST(req) {
+  await mongooseConnect();
   const body = await req.json();
-  const { name } = body;
+  const { name, parentCategory } = body;
   const categoryDoc = await Category.create({
     name,
+    parentCategory,
   });
 
   return NextResponse.json(categoryDoc);
+}
+
+export async function GET(req) {
+  await mongooseConnect();
+  const categories = await Category.find().populate("parentCategory");
+  return NextResponse.json(categories);
+}
+
+export async function PUT(req) {
+  await mongooseConnect();
+  const body = await req.json();
+  const { name, parentCategory, _id } = body;
+  await Category.updateOne({ _id }, { name, parentCategory });
+  return NextResponse.json(true);
 }
--- app/categories/page.js
+++ app/categories/page.js
@@ -1,20 +1,53 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 export default function Categories() {
   const [name, setName] = useState("");
+  const [editedCategory, setEditedCategory] = useState(null);
+  const [parentCategory, setParentCategory] = useState("");
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetchCategories();
+  }, []);
+
+  function fetchCategories() {
+    axios.get("/api/categories").then((result) => {
+      setCategories(result.data);
+    });
+  }
 
   async function saveCategory(e) {
     e.preventDefault();
-    await axios.post("/api/categories", { name });
+    const data = { name, parentCategory };
+    if (editedCategory) {
+      data._id = editedCategory._id;
+      await axios.put("/api/categories", data);
+      setEditedCategory(null);
+    } else {
+      await axios.post("/api/categories", data);
+    }
+
     setName("");
+    fetchCategories();
   }
+
+  async function editCategory(category) {
+    setEditedCategory(category);
+    setName(category.name);
+    setParentCategory(category.parentCategory?._id);
+  }
+
   return (
     <>
       <h1>Categories</h1>
-      <label>New category name</label>
+      <label>
+        {editedCategory
+          ? `Edit category ${editedCategory.name}`
+          : "Create new category "}
+      </label>
 
       <form onSubmit={saveCategory} className="flex gap-1">
         <input
@@ -24,10 +57,49 @@ export default function Categories() {
           placeholder={"Category name"}
           className="my-0"
         />
+        <select
+          value={parentCategory}
+          onChange={(e) => setParentCategory(e.target.value)}
+          className="my-0"
+        >
+          <option value="">No parent category</option>
+          {categories.length > 0 &&
+            categories.map((category) => (
+              <option value={category._id}>{category.name}</option>
+            ))}
+        </select>
         <button type="submit" className="btn-primary py-1">
           Save
         </button>
       </form>
+
+      <table className="basic mt-4">
+        <thead>
+          <tr>
+            <td>Category name</td>
+            <td>Parent category</td>
+            <td></td>
+          </tr>
+        </thead>
+        <tbody>
+          {categories.length > 0 &&
+            categories.map((category) => (
+              <tr>
+                <td>{category.name}</td>
+                <td>{category?.parentCategory?.name}</td>
+                <td>
+                  <button
+                    onClick={() => editCategory(category)}
+                    className="btn-primary mr-1"
+                  >
+                    Edit
+                  </button>
+                  <button className="btn-primary">Delete</button>
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
     </>
   );
 }
